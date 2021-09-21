11c415d329965d1ca745310b13be02f54b15153b
funkye93 <funkye93@gmail.com>
2021-09-20 22:31:41 -0400

    Add XRig to Main Scene
    Also starting work on phone

---
+++ Assets/Scripts/Telephone.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Telephone : MonoBehaviour {
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	public void Press0() {
+		Debug.Log("0 Pressed");
+	}
+
+	public void Press1() {
+		Debug.Log("1 Pressed");
+	}
+
+	public void PressCall() {
+		Debug.Log("Call Pressed");
+	}
+}
